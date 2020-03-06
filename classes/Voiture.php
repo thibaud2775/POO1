9b778201b0ff97448e5f7134d178c8dbb050afb7
Thibaud Decroo <tdecroo@gmail.com>
2020-03-06 10:14:37 +0100

    création des attributs de la classe Voiture

---
+++ classes/Voiture.php
@@ -0,0 +1,14 @@
+<?php
+
+// par convention les fichiers qui contiennent des classes ont un Nom qui commence par majuscule
+
+class Voiture 
+{
+    public $couleur;
+    public $vitesse = 0;
+    public $masse;
+    public $nbrRoues = 4;
+    public $energie;
+    public $marque;
+    public $modele;
+}
