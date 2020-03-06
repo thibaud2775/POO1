4c063516798aa8421195a65843074ea332417980
Thibaud Decroo <tdecroo@gmail.com>
2020-03-06 11:22:19 +0100

    getters et setters

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -10,12 +10,48 @@ class Voiture
     public $nbrRoues = 4;
     public $energie;
     public $marque;
-    public $modele;
+    public $modele
 
-    public function __construct(string $c)
+    public function __construct(string $c,int $m, string $e, string $mq, string $mod)
     {
         $this->couleur = $c;
+        $this->masse = $m;
+        $this->energie= $e;
+        $this->marque = $m;
+        $this->modele = $mod;
     }
+    public function getCouleur(): string {
+        return $this->couleur;
+    }
+    public function setCouleur($value) : void {
+        $this->couleur = $value;
+    }
+    public function getMasse(): int {
+        return $this->masser;
+    }
+    public function setMasse($value) : void {
+        $this->masse = $value;
+    }
+
+    public function getEnergie(): string {
+        return $this->energie;
+    }
+    public function setEnergie($value) : void {
+        $this->energie = $value;
+    }
+    public function getMarque(): string {
+        return $this->marque;
+    }
+    public function setMarque($value) : void {
+        $this->marque = $value;
+    }
+    public function getModele(): string {
+        return $this->modele;
+    }
+    public function setModele($value) : void {
+        $this->mmodele = $value;
+    }
+
     public function __destruct()
     {
         echo '<p>ta caisse est moche</p>';
--- index.php
+++ index.php
@@ -5,5 +5,5 @@ require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoLoader');
 
 //on instancie l'objet voiture
-$voiture = new Voiture('Moutarde');
+$voiture = new Voiture('Moutarde',943,'SP95','Renault','Twingo');
 var_dump($voiture);
